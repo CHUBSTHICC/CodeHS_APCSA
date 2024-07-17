36cf225d0243c04669783479b04b43d22144ecf4
Jacob Mastin <112589033+CHUBSTHICC@users.noreply.github.com>
2024-07-17 11:00:52 -0700

    Create 1.4.6 Tshirt.java

---
+++ Unit-1/1.4.6 Tshirt.java
@@ -0,0 +1,15 @@
+public class Tshirt
+{
+    public static void main(String[] args)
+    {
+        int cost = 22;
+        System.out.print("The original price is $");
+        System.out.println(cost);
+        cost++;
+        System.out.print("The price with customization is $");
+        System.out.println(cost);
+        cost--;
+        System.out.print("The original price is $");
+        System.out.println(cost);
+    }
+}

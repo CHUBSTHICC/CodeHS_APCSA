e4c4c319fc87ddaee3f37961e288352c5182a68b
Jacob Mastin <112589033+CHUBSTHICC@users.noreply.github.com>
2024-07-17 12:03:53 -0700

    Create 2.1.9 ConsoleGame.java

---
+++ Unit-2/2.1.9 ConsoleGame.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * The ConsoleGame class represents a console game.
+ * 
+ * Add the instance variables you think a console game would need
+ */ 
+
+public class ConsoleGame
+{
+    private String name;
+    private double cost;
+    private boolean online;
+    private int numPeople;
+    private boolean connection;
+}

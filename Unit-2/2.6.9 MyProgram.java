9085618a7a9bed0eede3a22ef573db5915ecde47
Jacob Mastin <112589033+CHUBSTHICC@users.noreply.github.com>
2024-07-17 14:11:39 -0700

    Create 2.6.9 MyProgram.java

---
+++ Unit-2/2.6.9 MyProgram.java
@@ -0,0 +1,11 @@
+public class MyProgram
+{
+    public static void main(String[] args)
+    {
+        //Build two different Student objects, then call all 3 methods on them!
+        Student stu1 = new Student("Jerry Werry", 62, 0.2);
+        System.out.println(stu1);
+        Student vinny = new Student();
+        System.out.println(vinny);
+    }
+}

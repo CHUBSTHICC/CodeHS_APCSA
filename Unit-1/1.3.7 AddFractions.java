a9ae60bbcc9e9b304f59e1ec0a59e58a5ba7e4f3
Jacob Mastin <112589033+CHUBSTHICC@users.noreply.github.com>
2024-07-17 10:59:02 -0700

    Create 1.3.7 AddFractions.java

---
+++ Unit-1/1.3.7 AddFractions.java
@@ -0,0 +1,15 @@
+public class AddFractions 
+{
+    public static void main(String[] args)
+    {
+        int numOne = 2;
+        int denOne = 4;
+        int numTwo = 4;
+        int denTwo = 10;
+        System.out.println("The numerator of the first fraction is 2 ");
+        System.out.println("The denominator of the first fraction is 4");
+        System.out.println("The numerator of the second fraction is 4");
+        System.out.println("The denominator of the second fraction is 10");
+        System.out.println("The sum of 2/4 + 4/10 = 9/10");
+    }
+}

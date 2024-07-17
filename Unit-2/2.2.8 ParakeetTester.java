b7c4f0cdb3706361506da4acebc644951f001a73
Jacob Mastin <112589033+CHUBSTHICC@users.noreply.github.com>
2024-07-17 12:08:30 -0700

    Create 2.2.8 ParakeetTester.java

---
+++ Unit-2/2.2.8 ParakeetTester.java
@@ -0,0 +1,8 @@
+public class ParakeetTester
+{
+    public static void main(String[] args)
+    {
+        Parakeet budgie = new Parakeet("Budgie", "Red");
+        System.out.println(budgie);
+    }
+}

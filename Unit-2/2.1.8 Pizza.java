c9944a9f74b41a9227ef274c1d1daf34f0b694c5
Jacob Mastin <112589033+CHUBSTHICC@users.noreply.github.com>
2024-07-17 12:03:12 -0700

    Create 2.1.8 Pizza.java

---
+++ Unit-2/2.1.8 Pizza.java
@@ -0,0 +1,9 @@
+public class Pizza
+{
+    // Add your instance variables here
+    private char size;
+    private String toppings;
+    private double cost;
+    private boolean cheese;
+    private String name;
+}

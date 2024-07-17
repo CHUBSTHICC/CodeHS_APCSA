bb6d5a97f5563eafee038cb02cb49f430d677783
Jacob Mastin <112589033+CHUBSTHICC@users.noreply.github.com>
2024-07-17 14:16:57 -0700

    Create 2.8.7 AnimalTester.java

---
+++ Unit-2/2.8.7 AnimalTester.java
@@ -0,0 +1,12 @@
+public class AnimalTester
+{
+    public static void main(String[] args)
+    {
+        Animal riverOtter = new Animal("North American River Otter", "rivers", "fish", 9);
+        System.out.println(riverOtter);
+       
+        Animal lion = new Animal ("Lion", "sub-saharan Africa", "meat", 12);
+        System.out.println(lion);
+       
+    }
+}

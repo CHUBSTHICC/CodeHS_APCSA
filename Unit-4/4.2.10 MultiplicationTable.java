4febcf916b19ea686ffc2ffd3f0092abb0d3492c
Jacob Mastin <112589033+CHUBSTHICC@users.noreply.github.com>
2024-07-19 09:56:37 -0700

    Create 4.2.10 MultiplicationTable.java

---
+++ Unit-4/4.2.10 MultiplicationTable.java
@@ -0,0 +1,12 @@
+public class MultiplicationTable
+{
+    public static void main(String[] args)
+    {
+        // Your code goes here!
+        for(int x = 1; x <= 10; x++)
+        {
+            int sum = 4 * x;
+            System.out.println(4 + " * " + x + " = " + sum);
+        }
+    }
+}

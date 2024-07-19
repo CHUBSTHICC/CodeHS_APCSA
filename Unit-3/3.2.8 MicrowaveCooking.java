3883dca02af9a2b9b85ab01f3fc36207fda5f37d
Jacob Mastin <112589033+CHUBSTHICC@users.noreply.github.com>
2024-07-19 09:31:19 -0700

    Create 3.2.8 MicrowaveCooking.java

---
+++ Unit-3/3.2.8 MicrowaveCooking.java
@@ -0,0 +1,19 @@
+public class MicrowaveCooking
+{
+    public static void main(String[] args)
+    {
+       // Generate a random number of seconds
+        int rand = (int)(Math.random() * (151) + 90);
+       // Print the number of seconds
+       System.out.println("Microwaving for " + rand + " seconds");
+       // Use two if statements to print the temperature
+       if (rand <= 120)
+       {
+           System.out.println("Rolls will be the right temperature!");
+       }
+       if (rand > 120)
+       {
+           System.out.println("Rolls will be boiling hot!");
+       }
+    }
+}

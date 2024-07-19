7a3643782520e2bda9a6d065e5d54800cb8697c0
Jacob Mastin <112589033+CHUBSTHICC@users.noreply.github.com>
2024-07-19 09:56:07 -0700

    Create 4.2.9 Loops.java

---
+++ Unit-4/4.2.9 Loops.java
@@ -0,0 +1,14 @@
+public class Loops
+{
+    public static void main(String[] args)
+    {
+        // Run this code first to see what it does. 
+        // Then replace the for loop with an equivalent while loop.
+        int count = 0;
+        while (count <= 22)
+        {
+            System.out.println(count);
+            count += 3;
+        }
+    } 
+}
